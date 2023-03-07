236ee5e0ab90fbdf8912dc3430cb5dfde206f387
irvantaufik28 <irvantaufik28@gmail.com>
2023-03-07 12:57:11 +0700

    cta -banner

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -2,16 +2,20 @@ import React from 'react'
 import Banner from '../components/homepage/Banner'
 import BestCar from '../components/homepage/BestCar'
 import WhyUs from '../components/homepage/WhyUs'
+import Testimonial from '../components/homepage/Testimonial'
+import CtaBanner from '../components/homepage/CtaBanner'
 
 
 
 export default function HomePage() {
   return (
     <>
-    <Banner />
-    <BestCar />
-    <WhyUs />
-    
+      <Banner />
+      <BestCar />
+      <WhyUs />
+      {/* <Testimonial /> */}
+      <CtaBanner />
+
     </>
   )
 }
