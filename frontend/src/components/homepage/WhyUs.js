6d1868862d904864037c339fd70a3f5b5776e62d
irvantaufik28 <irvantaufik28@gmail.com>
2023-03-07 11:19:39 +0700

    why us section fe

--- frontend/src/components/homepage/WhyUs.js
+++ frontend/src/components/homepage/WhyUs.js
@@ -11,35 +11,45 @@ export default function WhyUs() {
             <div className="container">
                 <div className='title-why-us'>
                     <h1>Why Us?</h1>
-                    <p>Mengapa harus pilih Binar Car Rental</p>
+                    <p>Mengapa harus pilih Binar Car Rental?</p>
                 </div>
-
-                <div className="row">
+                <div className="row why-us">
                     <div className="col-md-3">
-                        <img src={icon_complete} alt="icon_complete" />
-                        <h3>Mobil Lengkap</h3>
-                        <p>Tersedia banyak pilihan mobil, kondisi masih baru, bersih dan terawat</p>
-                    </div>
-
-
-                    <div className="col-md-3">
-                        <img src={icon_price} alt="icon_price" />
-                        <h3>Harga Murah</h3>
-                        <p>Harga murah dan bersaing, bisa bandingkan harga kami dengan rental mobil lain</p>
+                        <div className="card">
+                            <img src={icon_complete} alt="icon_complete" />
+                            <div className="card-body">
+                                <h5 className="card-title">Mobil Lengkap</h5>
+                                <p className="card-text">Tersedia banyak pilihan mobil, kondisi masih baru, bersih dan terawat</p>
+                            </div>
+                        </div>
                     </div>
                     <div className="col-md-3">
-                        <img src={icon_24hrs} alt="icon_24hrs" />
-                        <h3>Layanan 24 jam</h3>
-                        <p>Siap melayani kebutuhan Anda selama 24 jam nonstop. Kami juga tersedia di akhir minggu</p>
+                        <div className="card">
+                            <img src={icon_price} alt="icon_price" />
+                            <div className="card-body">
+                                <h5 className="card-title">Harga Murah</h5>
+                                <p className="card-text">Harga murah dan bersaing, bisa bandingkan harga kami dengan rental mobil lain</p>
+                            </div>
+                        </div>
                     </div>
-
                     <div className="col-md-3">
-                        <img src={icon_professional} alt="icon_professional" />
-                        <h3>Sopir Profesional</h3>
-                        <p>Sopir yang profesional, berpengalaman, jujur, ramah dan selalu tepat waktu</p>
+                        <div className="card">
+                            <img src={icon_24hrs} alt="icon_24hrs" />
+                            <div className="card-body">
+                                <h5 className="card-title">Layanan 24 jam</h5>
+                                <p className="card-text">Siap melayani kebutuhan Anda selama 24 jam nonstop. Kami juga tersedia di akhir minggu</p>
+                            </div>
+                        </div>
                     </div>
-
-
+                    <div className="col-md-3">
+                        <div className="card">
+                            <img src={icon_professional} alt="icon_professional" />
+                            <div className="card-body">
+                                <h5 className="card-title">Sopir Profesional</h5>
+                                <p className="card-text">Sopir yang profesional, berpengalaman, jujur, ramah dan selalu tepat waktu</p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
